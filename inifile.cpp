cc2f6c804d88740bcaf55f4e5c4f5984fa326fdf
Sean <sean@Ubuntu.(none)>
2010-12-23 16:06:27 -0500

    Reworked ini handling to remove the need for glib

--- inifile.cpp
+++ inifile.cpp
@@ -1,5 +1,10 @@
-#include <glib.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "pcrecpp.h"
 #include "inifile.h"
+
+using namespace std;
 
 IniFile::IniFile(const QString &filename)
 {
@@ -11,7 +16,7 @@ QStringList IniFile::groups()
 	QStringList groups;
 	QHash<QString, QHash<QString, QString> >::const_iterator groupIterator = this->configItems.constBegin();
 
-	for (groupIterator = this->configItems.constBegin(); groupIterator!= this->configItems.constEnd(); ++groupIterator) {
+	for (groupIterator = this->configItems.constBegin(); groupIterator!= this->configItems.constEnd(); groupIterator++) {
 		groups.append(groupIterator.key());
 	}
 
@@ -26,7 +31,7 @@ QStringList IniFile::keys(const QString &groupName)
 		QHash<QString, QString> groupHash = this->configItems.value(groupName);
 		QHash<QString, QString>::const_iterator keyIterator = groupHash.constBegin();
 
-		for (keyIterator = groupHash.constBegin(); keyIterator != groupHash.constEnd(); ++keyIterator) {
+		for (keyIterator = groupHash.constBegin(); keyIterator != groupHash.constEnd(); keyIterator++) {
 			keys.append(keyIterator.key());
 		}
 	}
@@ -45,86 +50,43 @@ void IniFile::setValue(QString groupName, QString keyName, QString value)
 
 void IniFile::save(const QString &filename)
 {
-	GKeyFile *keyFile = g_key_file_new();
-
-	if (g_key_file_load_from_file(keyFile, filename.toLocal8Bit().constData(), G_KEY_FILE_NONE, 0 /* no error handling */) == FALSE) {
-		g_key_file_free(keyFile);
-		return;
-	}
-
-	// Take all the values from the IniFile and put it into
-	// the GKeyFile structure.
+    ofstream ini;
+    ini.open(filename.toAscii());
 	QStringList groups = this->groups();
 
 	QStringList::const_iterator groupIter;
-	for (groupIter = groups.constBegin(); groupIter != groups.constEnd(); ++groupIter) {
+	for (groupIter = groups.constBegin(); groupIter != groups.constEnd(); groupIter++) {
 		QString groupName = *groupIter;
 		QStringList keys = this->keys(groupName);
+        ini << "[" << groupName.toStdString() << "]\n";
 
 		QStringList::const_iterator keyIter;
-		for (keyIter = keys.constBegin(); keyIter != keys.constEnd(); ++keyIter) {
+		for (keyIter = keys.constBegin(); keyIter != keys.constEnd(); keyIter++) {
 			QString keyName = *keyIter;
 			QString value = this->value(groupName, keyName);
-
-			g_key_file_set_value(keyFile, groupName.toLocal8Bit().constData(), keyName.toLocal8Bit().constData(), value.toLocal8Bit().constData());
+            ini << keyName.toStdString() << "=" << value.toStdString() << "\n";
 		}
 	}
-
-	// Save our GKeyFile to a file
-	gsize keyFileDataLength;
-	gchar *keyFileData = g_key_file_to_data(keyFile, &keyFileDataLength, 0 /* no error handler */);
-
-	QFile file(filename);
-	if (file.open(QIODevice::WriteOnly | QIODevice::Text)) {
-		QTextStream out(&file);
-		out << keyFileData;
-	}
-
-	g_free(keyFileData);
-	g_key_file_free(keyFile);
+    ini.close();
 }
 
 void IniFile::parseFile(const QString &filename)
 {
-	gchar **groups;
-	gsize g, groupCount;
+	pcrecpp::RE Group("^\\[(.*)\\]$");
+	pcrecpp::RE ItemVal("^([^=]+)=(.*)$");
 
-	GKeyFile *keyFile = g_key_file_new();
-
-	if (g_key_file_load_from_file(keyFile, filename.toLocal8Bit().constData(), G_KEY_FILE_NONE, 0 /* no error handling */) == FALSE) {
-		g_key_file_free(keyFile);
-		return;
-	}
-
-	// Now we get the groups from the keyFile.
-	// For each group, we get they keys, and the value for each key.
-	//
-	// The groupName is the top-level hashkey.
-	// The keyName is bottom-level hashkey.
-	// The value is the bottom-level hashvalue.
-	if ((groups = g_key_file_get_groups(keyFile, &groupCount)) != 0) {
-		for (g = 0; g < groupCount; g++) {
-			gchar **keys;
-			gsize k, keyCount;
-
-			if ((keys = g_key_file_get_keys(keyFile, groups[g], &keyCount, 0 /* no error handler */)) != 0) {
-				QHash<QString, QString> groupItems;
-
-				for (k = 0; k < keyCount; k++) {
-					gchar *value;
-
-					if ((value = g_key_file_get_value(keyFile, groups[g], keys[k], 0)) != 0) {
-						groupItems.insert(QString(keys[k]), QString(value));
-						g_free(value);
-					}
-				}
-
-				g_strfreev(keys);
-				this->configItems.insert(QString(groups[g]), groupItems);
-			}
-		}
-	}
-
-	g_strfreev(groups);
-	g_key_file_free(keyFile);
+    ifstream ini(filename.toAscii());
+    string temp,group,item,value;
+    while (getline(ini,temp)) {
+        if (Group.PartialMatch(temp,&group)) {
+        }
+        else if (ItemVal.PartialMatch(temp,&item,&value)) {
+            if (this->configItems.contains(QString::fromStdString(group)) == false) {
+                QHash<QString, QString> insertme;
+                this->configItems.insert(QString::fromStdString(group),insertme);
+            }
+            this->configItems[QString::fromStdString(group)][QString::fromStdString(item)] = QString::fromStdString(value);
+        }
+    }
 }
+
